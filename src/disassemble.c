6d219516f4ededd4a1e8e06929200f73b500bb3f
arushrmaiya <arushmaiyar@gmail.com>
2025-06-21 14:53:44 -0500

    added some code

--- src/disassemble.c
+++ src/disassemble.c
@@ -4,23 +4,31 @@
 
 int openbuffer(int argc, char* fname){
 
-    printf("hi\n");
     FILE* f = fopen(fname, "rb");
     if(!f){
         printf("INVALID FILE");
         exit(1);
+    }
+
+    fseek(f, 0L, SEEK_END);
+    int fsize = ftell(f);
+    fseek(f, 0L, SEEK_SET);
+
+    unsigned char* buffer = (unsigned char *)malloc(fsize*sizeof(unsigned char));
+    
+    int pc = 0;
+
+    fread(buffer, fsize, 1, f);
+
+    while(pc < fsize){
+        
     }
     return 0;
 
 }
 
 void disassemble(unsigned char* buffer, int pc){
-    // unsigned char* code = &buffer;
 
-    // switch(*code){
-    //     case 0x00: printf("NOP\n"); break;
-
-    // }
     return;
 }
 
